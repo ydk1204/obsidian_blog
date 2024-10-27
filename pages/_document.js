a94a70c2887706465dcc355d5eacf007dc91931b
ydk1204 <ejrbdi@naver.com>
2024-10-27 15:43:47 +0900

    구글 서치콘솔 소유권 확인

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="ko">
       <Head>
         <link rel="sitemap" type="application/xml" href="/sitemap.xml" />
+        <meta name="google-site-verification" content="T5vTCzLN624DPglsqSZcxXYGii9cxtjC7e0BQIpazxY" />
       </Head>
       <body>
         <Main />
