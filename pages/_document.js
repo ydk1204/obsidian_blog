e95085075ecfc32518114fbcc181f27822a1db71
ydk1204 <ejrbdi@naver.com>
2024-10-28 14:15:39 +0900

    ms bing, naver 웹마스터 도구 소유권 확인 추가

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,8 @@ export default function Document() {
       <Head>
         <link rel="sitemap" type="application/xml" href="/sitemap.xml" />
         <meta name="google-site-verification" content="T5vTCzLN624DPglsqSZcxXYGii9cxtjC7e0BQIpazxY" />
+        <meta name="msvalidate.01" content="A15390E60B0057715C784052E3E98F10" />
+        <meta name="naver-site-verification" content="1e79b713fa8d4963c8dfacdbe6b0cc7cd301a93a" />
       </Head>
       <body>
         <Main />
